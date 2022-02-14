5c7321254b010afac4c577e2014fb26808c5a46d
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-14 13:02:00 +0545

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import { faMusic } from  '@fortawesome/free-solid-svg-icons';
 const Nav = ({ setLibraryStatus,libraryStatus}) => {
     return (
         <nav>
-            <h1>F*ck Uh!</h1>
+            <h1>Musicy</h1>
             <button onClick={()=>setLibraryStatus(!libraryStatus)}>
                 Library
                  <FontAwesomeIcon icon={faMusic}></FontAwesomeIcon>
